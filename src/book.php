cb63b99346c7cadcdd51b89955441a9d298835d1
Ihab Zeedia <ihab.zeedia@gmail.cm>
2019-06-27 09:00:19 -0700

    -change logic that allows system to recognize the complete field by placing instrument as second

--- src/book.php
+++ src/book.php
@@ -6,21 +6,26 @@ namespace Stanford\AppointmentScheduler;
 
 
 try {
+
     $data = $module->sanitizeInput();
     if ($data['email' . $module->getSuffix()] == '' || $data['name' . $module->getSuffix()] == '' || $data['mobile' . $module->getSuffix()] == '') {
         throw new \LogicException('Data cant be missing');
     } else {
 
         $data['participant_status' . $module->getSuffix()] = RESERVED;
-
+        $eventId = $module->getReservationEventIdViaSlotEventId($data['event_id']);
         /**
          * let mark it as complete so we can send the survey if needed.
          * Complete status has different naming convension based on the instrument name. so you need to get instrument name and append _complete to it.
          */
-        $data[$Proj->lastFormName . '_complete'] = REDCAP_COMPLETE;
-        $data['event_id'] = $module->getReservationEventIdViaSlotEventId($data['event_id']);
-        $data['redcap_event_name'] = \REDCap::getEventNames(true, true, $data['event_id']);
-        $data['record_id'] = $module->getNextRecordsId($data['event_id'], PROJECT_ID);
+        $labels = \REDCap::getValidFieldsByEvents(PROJECT_ID, array($eventId));
+        $completed = preg_grep('/_complete$/', $labels);
+        $second = array_slice($completed, 1, 1);  // array("status" => 1)
+
+        $data[$second] = REDCAP_COMPLETE;
+
+        $data['redcap_event_name'] = \REDCap::getEventNames(true, true, $eventId);
+        $data['record_id'] = $module->getNextRecordsId($eventId, PROJECT_ID);
         $response = \REDCap::saveData('json', json_encode(array($data)));
         if (empty($response['errors'])) {
             //$module->notifyUser($data);
