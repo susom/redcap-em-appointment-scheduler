25afbfc90e33e0dfb361d7a2d4ddd21586a33c45
Ihab Zeedia <ihab.zeedia@gmail.cm>
2020-05-18 12:51:22 -0700

    style the landing page.

--- src/types.php
+++ src/types.php
@@ -15,58 +15,72 @@ $managerURL = $module->getUrl('src/manager.php', false, false, true) . '&project
 ?>
 
 
-    <?php
+<?php
 
 //JS and CSS with inputs URLs
 require_once 'urls.php';
 ?>
-<link rel="stylesheet" href="<?php echo $module->getUrl('src/css/types.css', true, true) ?>">
+<div id="brandbar">
     <div class="container">
-        <nav class="navbar navbar-expand-sm bg-light navbar-light">
-            <?php
-            if (defined('USERID')) {
-                ?>
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Logged in
-                            as: <?php echo(defined('USERID') ? USERID : ' NOT LOGGED IN') ?></a>
-                    </li>
-                </ul>
-                <?php
-            }
-            ?>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-                <?php
-                if (defined('USERID')) {
+        <div class="row">
+            <div class="col-6">
+                <a href="http://www.stanford.edu"><img
+                            src="https://www-media.stanford.edu/su-identity/images/brandbar-stanford-logo@2x.png"
+                            alt="Stanford University" width="152" height="23"></a>
+            </div>
+            <div class="col-6">
+                <nav class="navbar navbar-expand-sm navbar-dark">
+                    <?php
+                    if (defined('USERID')) {
                         ?>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="manage nav-link" href="#">Manage my Appointments</a>
-                        </li>
-                        <?php
-                        if ($module::isUserHasManagePermission()) {
-                            ?>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link " href="<?php echo $managerURL ?>" id="navbarDropdown">
-                                    Go to Manager Page
-                                </a>
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Logged in
+                                    as: <?php echo(defined('USERID') ? USERID : ' NOT LOGGED IN') ?></a>
                             </li>
+                        </ul>
+                        <?php
+                    }
+                    ?>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+                        <?php
+                        if (defined('USERID')) {
+                            ?>
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="manage nav-link" href="#">Manage my Appointments</a>
+                                </li>
+                                <?php
+                                if ($module::isUserHasManagePermission()) {
+                                    ?>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link " href="<?php echo $managerURL ?>" id="navbarDropdown">
+                                            Go to Manager Page
+                                        </a>
+                                    </li>
+                                    <?php
+                                }
+                                ?>
+                            </ul>
                             <?php
                         }
                         ?>
-                    </ul>
-                        <?php
-                    }
-                ?>
+                    </div>
+                </nav>
             </div>
-        </nav>
+        </div>
     </div>
+</div>
+<link rel="stylesheet" href="<?php echo $module->getUrl('src/css/types.css', true, true) ?>">
+<div class="container">
 
-    <div class="container">
-        <?php
-        foreach ($instances as $instance) {
-            $title = $instance['title'];
-            if (isset($_GET['complementary']) && $_GET['complementary'] == 'true') {
+</div>
+
+<div class="container">
+    <?php
+    foreach ($instances as $instance) {
+        $title = $instance['title'];
+        if (isset($_GET['complementary']) && $_GET['complementary'] == 'true') {
                 $slotsEventId = $instance['survey_complementary_slot_event_id'];
                 $reservationEventId = $instance['survey_complementary_reservation_event_id'];
             } else {
