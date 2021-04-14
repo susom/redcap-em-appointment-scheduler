c39c89e868af94419a24c29e29a7bf03181e57b3
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-04-14 10:15:06 -0700

    test comment.

--- src/list.php
+++ src/list.php
@@ -9,6 +9,7 @@ $eventId = filter_var($_GET['event_id'], FILTER_SANITIZE_NUMBER_INT);
 $data = $module->getMonthSlots($eventId);
 $url = $module->getUrl('src/calendar.php', true, true);
 $instance = $module->getEventInstance();
+// test comment
 ?>
     <style>
         @media (max-width: 550px) {
